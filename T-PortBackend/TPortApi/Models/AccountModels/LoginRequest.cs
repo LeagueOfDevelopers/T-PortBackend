6b5cf054dae814739e970abca031ce5c96c374a3
David <khuriev.david@gmail.com>
2019-02-01 20:42:43 +0300

    new phone number validation regular expression added

--- T-PortBackend/TPortApi/Models/AccountModels/LoginRequest.cs
+++ T-PortBackend/TPortApi/Models/AccountModels/LoginRequest.cs
@@ -5,7 +5,7 @@ namespace TPortApi.Models.AccountModels
     public class LoginRequest
     {
         [Required]
-        [RegularExpression(@"^\+[1-9][1-9]?[1-9]?\d{3}\d{3}\d{4}$", ErrorMessage = "Invalid phone number")]
+        [RegularExpression(@"^\+[1-9][1-9]?[1-9]?[ ]?[(]?\d{3}[)]?[ ]?\d{3}[ ]?\d{2}[ ]?\d{2}$", ErrorMessage = "Invalid phone number")]
         public string Phone { get; set; }
     }
 }
